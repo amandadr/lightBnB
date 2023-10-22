9d015d45b936e4e1347ea01221e368b9b28ec8fa
amandadr <amandadroy@gmail.com>
2023-10-22 18:38:38 -0300

    fix getAllProperties using avg_rating option in search

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -129,16 +129,17 @@ const getAllProperties = function (options, limit = 10) {
       queryParams.push(Number(options.maximum_price_per_night)*100);
       queryString += `AND cost_per_night <= $${queryParams.length} `;
   } 
-  if (options.minimum_rating) {
-      queryParams.push(Number(options.minimum_rating));
-      queryString += `AND property_reviews.rating >= $${queryParams.length} `;
-  }
 
   // 4
-  queryParams.push(limit);
   queryString += `
-  GROUP BY properties.id
-  ORDER BY cost_per_night
+  GROUP BY properties.id `;
+  if (options.minimum_rating) {
+    queryParams.push(Number(options.minimum_rating));
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+  queryParams.push(limit);
+  queryString +=
+  `ORDER BY cost_per_night 
   LIMIT $${queryParams.length};
   `;
 
